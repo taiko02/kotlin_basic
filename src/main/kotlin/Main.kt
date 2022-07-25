63980f5237857fd542cdb23e54f152dfa7757862
tomi <tommycodingjj@gmail.com>
2022-07-25 10:50:56 +0700

    add notes.txt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,15 @@
 fun main () {
-    val number: Int
-    number = 12
-//    number = 5
 
-    var number2: Int
-    number2 = 19
-    number2 = 12
+    val number = 2
+    val result = when (number){ //when bisa dimasukan kedalam variable
+        1 -> { //untuk value yang banyak bisa menggunakan {}
+            println("bernilai 1")
+            print("betul number 1")
+        }
+        2 -> "Bernilai 2" //bisa langsung ada nilainya
+        3 -> print("number bernilai 3")
+        else -> print("number tidak ada yang cocok") //wajib menambahkan else jika ada variable
+    }
+    println(result)
+
 }

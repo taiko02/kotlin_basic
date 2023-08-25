c3efd9a2418b456933da4d3e7f33b21f07ecc6b5
Tomi Saputra <tommycodingjj@gmail.com>
2023-08-25 12:13:57 +0700

    val dan var

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,9 @@
-fun  main(){
-    val array: Array<Any>
-    array = arrayOf(2,3,4,5,"sasa",true)
+fun main(args: Array<String>) {
+    // val tidak bisa di assign/di ubah
+    val nama = "andik"
+    // var bisa di ubah atau di assign
+    var umur = 20
+    umur = 10
 
-    val list: List<Any>
-    list = listOf(2,3,4,5,"sasa",true)
-
-    val set: Set<Any>
-    set = setOf(2,3,4,5,"sasa",true)
-
-    //Atau cara simple nya dibawah ini
-    val data = listOf<Any>("string",12,"mantap") //Tambahkan tipe data dalam <> untuk deklarisikan isi array
-
-    // Contoh Map
-//    val data1:Map<String, String> //map harus dua tipe data <key, value>
-    val data1 = mapOf<String, Any>(
-        "a" to 12, //value harus sesuai dengan key dan value
-        "b" to "Banana"
-    )
-    println(data1.keys) //pemanggilan data bisa menggunakan getValue atau [](kurung siku)
-                        //atau values untuk tampilkan semua data
-                        //atau keys untuk melihat key apa saja dalam data
-
-//    val union = data.union(data1) //union : menggabungkan data lalu mencari nilai yang sama
-//    val union = data.intersect(data1) //intersect : hanya mencetak nilai yang sama
-//    println(data1[1])
+    println("Nama : $nama Umur : $umur")
 }

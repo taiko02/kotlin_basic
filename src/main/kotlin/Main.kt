93d83518b21d2def310478f07e81608ed7aae028
Tomi Saputra <tommycodingjj@gmail.com>
2023-08-25 13:11:56 +0700

    Array

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,24 @@
-fun main(args: Array<String>) {
-//    deklarasi tipe data
-//    deklrasi variable bisa di tambahkan setelah :
-    val nama: String = "anto"
-//    ini akan error karna string tapi value integer
-//    val  alamat: String = 20
 
-    println("Nama : $nama")
+fun main(args: Array<String>) {
+
+    var nama: Array<String> = arrayOf("hilman","makssss","anto")
+
+    // mengubah array menjadi list
+    println(nama.toList())
+
+    // cek panjang array
+    println(nama.size)
+
+    // memanggil salah satu data array
+    println(nama[2]) //anto
+    println(nama.get(1)) // bisa juga dengan get
+
+    // ubah data array
+    nama[0] = "hilda"
+    println(nama[0]) //hilda
+
+    nama.set(0,"hildoss") // atau bisa seperti ini
+    println(nama[0]) //hildoss
+
+
 }
